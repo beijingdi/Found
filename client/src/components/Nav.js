82ee37c3da07ef450cf32d584dd413196e443c59
Di Wu <dwu233@gmail.com>
2023-01-06 14:29:04 -0500

    add background picture from figma

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import './Nav.module.css';
 
 const Nav = (props) => {
   return(
-    <>
+    <nav>
     {/* <p>I am Nav</p> */}
-    <p> FOUND.</p>
-    <NavLink>About</NavLink>
-    <NavLink>Corporate Retreats</NavLink>
-    <NavLink>Gallery</NavLink>
-    <button>Book Now</button>
-    </>
+      <span> FOUND.</span>
+      <ul class="nav">
+        <li class="nav-item">
+          <NavLink>About</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink>Corporate Retreats</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink>Gallery</NavLink>
+        </li>
+        <li class="nav-item">
+          <button>Book Now</button>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
