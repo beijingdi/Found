22033f3caedeb55633946e4cc1e7aa2c53db30d7
Di Wu <dwu233@gmail.com>
2023-01-09 17:00:46 -0500

    add branding contents to main page

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -7,6 +7,22 @@ function MainPage(){
         <h1>Let's Thrive Together.</h1>
         <button>Book Now</button>
       </div>
+      <div>
+        <ul>
+          <li>
+            <h3>UNPLUG</h3>
+            <p>Unplug and enjoy time away from digital distractions, and focus more on the present.</p>
+          </li>
+          <li>
+            <h3>CONNECT</h3>
+            <p>Reconnext with ourselves, the ocean, nature and other like-minded individuals.</p>
+          </li>
+          <li>
+            <h3>RECHARGE</h3>
+            <p>Recharge through daily yoga, water sports and activities,coaching sessions,seasonal menus, and comfortable accommodation.</p>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
