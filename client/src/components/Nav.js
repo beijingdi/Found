6e9c9ea4c0c8d659ee2989179e8723daaabebaa9
Di Wu <dwu233@gmail.com>
2023-01-18 04:38:27 -0500

    add bootstrap styling to nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,26 +1,45 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import './Nav.module.css';
+import nav_classes from './Nav.module.css';
 
 const Nav = (props) => {
   return(
-    <nav>
-      <span> FOUND.</span>
-      <ul className="nav">
-        <li className="nav-item">
-          <NavLink>About</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink>Corporate Retreats</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink>Gallery</NavLink>
-        </li>
-        <li className="nav-item">
-          <button>Book Now</button>
-        </li>
-      </ul>
+    <>
+
+    <nav className="navbar navbar-expand-lg">
+
+      <a className={`${nav_classes.logo} navbar-brand`} href="/">FOUND.</a>
+
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Corporate Retreats</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Book Now</a>
+          </li>
+        </ul>
+      </div>
+     
+
+     
+          {/* <NavLink class="nav-item">About</NavLink>
+          <NavLink class="nav-item">Corporate Retreats</NavLink>
+          <NavLink class="nav-item">Gallery</NavLink>
+          <NavLink className="nav-item"><button>Book Now</button></NavLink> */}
+       
     </nav>
+    </>
   )
 }
 
