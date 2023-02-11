afe10e270e8725cb44069c9c97cc63e308edfd9b
Di Wu <dwu233@gmail.com>
2023-02-10 22:24:43 -0500

    implement bcrypt

--- server/db/db.js
+++ server/db/db.js
@@ -12,16 +12,31 @@ const { Pool } = require('pg');
 const pool = new Pool(dbParams);
 
 
-const getUserWithEmail = async (email) => {
+const findUser = async (email) => {
   try {
-    const result = await pool.query();
+    const result = await pool.query(
+      `SELECT * FROM users
+       WHERE email = $1`,[email]
+    );
     return result.rows[0];
   } 
   catch (err) {
-    console.log(err.message)  ;  
+    console.log(err);
   }
 }
 
+const createUser = async (email,password,name) => {
+  try {
+    const result = await pool.query(
+      `INSERT INTO users (email,password,name)
+       VALUES ($1, $2, $3)
+       RETURNING *;`,[email,password,name]
+    );
+  }
+  catch(err) {
+    console.log(err.message);
+  }
+}
 
 
 module.exports = {getUserWithEmail}
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,13 +4,26 @@ const router = express.Router();
 module.exports = (db) => {
 
   router.post('/login', (req, res) => {
-
+    const email = req.body.email;
+    const password = req.body.password;
+    const existingUser = db.getUserWithEmail(email);
+    if (!existingUser){
+      return res.json({status: 401, message: "User does not exist."});
+    }
+    if (existingUser.password != password) {
+      return res.json({status:401, message: "Incorrect Password."});
+    }
+    return res.json({status:200, id:user.id, name:user.name});
   });
 
+
+  
   router.post('/register', (req, res) => {
 
   });
 
 
 }
+
+
 
