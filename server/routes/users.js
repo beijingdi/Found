b0ed507cf8b2bd07488988167f07b07dc72c4f87
Di Wu <dwu233@gmail.com>
2023-02-20 01:54:40 -0500

    passport.js debug, finally working. A customFields parameter was missing for the local strategy

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,18 +11,42 @@ module.exports = (db) => {
   router.get('/all', async(req,res) => {
     try {
       const users = await db.allUsers();
+      // console.log(req.session);
+      // if (req.session.viewCount) {
+      //   req.session.viewCount = req.session.viewCount + 1;
+      // } else {
+      //   req.session.viewCount = 1;
+      // }
+      // res.json(req.session.viewCount);
       res.json(users);
     } catch(err) {
       console.log(err);
     }
   });
 
-  router.post(
-    '/login', 
-    passport.authenticate("local", {failuerRedirect:"/", failuerMessage: true})
-  );
+  router.post('/login', 
+  passport.authenticate('local', {failureRedirect: '/'}),
+  (req,res,next) => {
+    console.log("session");
+    console.log(req.session);
+    res.redirect("/");
+    // console.log(req.body);
+    // console.log("post request sent");
+    //   passport.authenticate("local",(err,user,info) => {
+    //     console.log("passport authentication starts");
+    //     console.log({err,user,info});
+     
+    //     if(err) throw err;
+       
+    //     if (!user) res.send("No User Exists");
 
-
+    //       req.logIn(user, err => {
+    //         if (err) throw err;
+    //         res.send('Successfully Authenticated');
+    //         console.log(req.user);
+    //       })
+    //   })(req,res,next);
+  });
 
   router.post('/register', async(req, res) => {
     const {name, email, password} = req.body;
@@ -34,8 +58,8 @@ module.exports = (db) => {
         return res.redirect('/');
       }
 
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password,salt);
+      // const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password,10);
       await db.createUser(name,email,hashedPassword);
       res.redirect("/");
     } catch (err) {
