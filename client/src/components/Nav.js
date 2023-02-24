228ac645e6756d2342ed129d74b99e885b5eb116
Di Wu <dwu233@gmail.com>
2023-02-24 08:08:42 -0500

    add link to login content to navbar dropdown

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -13,7 +13,7 @@ const Nav = (props) => {
   const loggedIn = !!user;
 
   //display login/registeration form
-  const [loginWindow, setLoginWindow] = useState(loggedIn);
+  const [loginWindow, setLoginWindow] = useState(false);
   const [registerWindow,setRegisterWindow] = useState(false);
 
   const openLogin = () => {
@@ -30,17 +30,15 @@ const Nav = (props) => {
 
   return(
     <>
-
+    
     <nav className="navbar-expand-lg">
-
-      <a className={`${nav_classes.logo} navbar-brand`} href="/">FOUND.</a>
-
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <a className={`${nav_classes.logo} navbar-brand`} href="#">FOUND.</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul className="navbar-nav">
+      <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auro">
           <li className="nav-item">
             <a className="nav-link" href="#">About</a>
           </li>
@@ -51,10 +49,18 @@ const Nav = (props) => {
             <a class="nav-link" href="#">Gallery</a>
           </li>
           {loggedIn && (
-            <li class="nav-item">
-              <strong>{user.name}</strong>
-            </li>
-
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {user.name}
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Book</a>
+                <a class="dropdown-item" href="#">My Bookings</a>
+                <a class="dropdown-item" href="#">Past Bookings</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Log Out</a>
+              </div>
+          </li>
           )}
           {!loggedIn && (
             <li class="nav-item">
@@ -81,11 +87,9 @@ const Nav = (props) => {
         closeForm={closeRegister}
       />
     )}
+  
+    </>);
 
-    {}
-    
-    </>
-  )
-}
+};
 
 export default Nav;
